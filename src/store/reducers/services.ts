edb7a30ec47f0bd8c318a15cfd4562a227b7b7ab
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-03-10 07:54:44 +0530

    Integrated the service list API - but dummy list showing first.

--- src/store/reducers/services.ts
+++ src/store/reducers/services.ts
@@ -1,4 +1,4 @@
-//import axios from 'utils/axios';
+import axios from 'utils/axios';
 import { createSlice } from '@reduxjs/toolkit';
 
 import { dispatch } from '../index';
@@ -9,7 +9,7 @@ import { services } from 'data/services';
 
 import { KeyedObject } from 'types/services';
 
-//import { Services as ServiceType } from 'types/services';
+import { Services as ServiceType } from 'types/services';
 
 const initialState: DefaultRootStateProps['service'] = {
   error: null,
@@ -36,7 +36,7 @@ const slice = createSlice({
   }
 });
 
-//const zkCloudEndpoint = 'http://test.com';
+const zkCloudEndpoint = 'http://localhost:8080/v1/cluster/1/default/';
 
 // Reducer
 export default slice.reducer;
@@ -44,28 +44,30 @@ export default slice.reducer;
 export function getServices() {
   return async () => {
     try {
-      //   console.log('Get services method called.');
-    //   const response = await axios.get(zkCloudEndpoint + 'services/list');
-    //   const servicesArr = response.data.services;
-    //   for (var i = 0; i < servicesArr.length; i++) {
-    //     var obj = servicesArr[i];
-    //     let service: ServiceType = {
-    //       name: obj.service,
-    //       podCount: obj.pod_count,
-    //       httpLatencyIn: {
-    //         p50: obj.http_latency_in.p50,
-    //         p90: obj.http_latency_in.p90,
-    //         p99: obj.http_latency_in.p99
-    //       },
-    //       httpReqThroughputIn: obj.http_req_throughput_in,
-    //       httpErrorRateIn: obj.http_error_rate_in,
-    //       inboundConns: obj.inbound_conns,
-    //       outboundConns: obj.outbound_conns
-    //     };
-    //     console.log(service);
-    //   }
-      //   console.log('Response for list of services is ', response);
-      dispatch(slice.actions.getServicesSuccess(services));
+      console.log('Get services method called.');
+      const response = await axios.get(zkCloudEndpoint + 'service/list');
+      const servicesArr = response.data.results;
+      let results: ServiceType[] = [];
+      for (var i = 0; i < servicesArr.length; i++) {
+        var obj = servicesArr[i];
+        let service: ServiceType = {
+          name: obj.service,
+          podCount: obj.pod_count,
+          httpLatencyIn: {
+            p50: obj.http_latency_in.p50,
+            p90: obj.http_latency_in.p90,
+            p99: obj.http_latency_in.p99
+          },
+          httpReqThroughputIn: obj.http_req_throughput_in,
+          httpErrorRateIn: obj.http_error_rate_in,
+          inboundConns: obj.inbound_conns,
+          outboundConns: obj.outbound_conns
+        };
+        results.push(service);
+      }
+      console.log(results);
+      //console.log('Response for list of services is ', response);
+      dispatch(slice.actions.getServicesSuccess(results));
     } catch (error) {
       dispatch(slice.actions.hasError(error));
     }
