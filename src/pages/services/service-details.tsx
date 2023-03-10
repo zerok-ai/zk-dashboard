d8f83d21220b90a4eafa0eb1adf5b0928f8da92c
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-03-10 15:22:02 +0530

    Added two dummy charts to service details page.

--- src/pages/services/service-details.tsx
+++ src/pages/services/service-details.tsx
@@ -1,21 +1,57 @@
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
+import IncomeAreaChart from 'sections/charts/IncomeAreaChart';
+import MainCard from 'components/MainCard';
+import { useState, useEffect } from 'react';
 
-// ==============================|| SAMPLE PAGE ||============================== //
+const ServiceDetailsPage = () => {
+  const [seriesData, setSeriesData] = useState([
+    {
+      name: '',
+      data: [0]
+    }
+  ]);
+  useEffect(() => {
+    setSeriesData([
+      {
+        name: 'Page Views',
+        data: [31, 40, 28, 51, 42, 109, 100]
+      },
+      {
+        name: 'Sessions',
+        data: [11, 32, 45, 32, 34, 52, 41]
+      }
+    ]);
+  }, []);
 
-const ServiceDetailsPage = () => (
-  <Grid container spacing={1} direction="column" alignItems="center" justifyContent="center" sx={{ minHeight: '50vh', py: 2 }}>
-    <Grid item xs={12}>
-      <Stack spacing={2} justifyContent="center" alignItems="center">
-        <Typography align="center" variant="h1">
-          Service Details Under Construction.
-        </Typography>
-        <Typography color="textSecondary" align="center" sx={{ width: '85%' }}>
-          Hey! Please check out this site later. We are working on it right now.
-        </Typography>
-      </Stack>
+  return (
+    <Grid container rowSpacing={1} columnSpacing={3}>
+      <Grid item xs={12} md={6} lg={6}>
+        <Grid item>
+          <Typography variant="h5">Example Data</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <MainCard content={false} sx={{ mt: 1.5 }}>
+            <Box sx={{ pt: 1, pr: 2 }}>
+              <IncomeAreaChart slot="week" series={seriesData}/>
+            </Box>
+          </MainCard>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={6} lg={6}>
+        <Grid item>
+          <Typography variant="h5">Other Example Data</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <MainCard content={false} sx={{ mt: 1.5 }}>
+            <Box sx={{ pt: 1, pr: 2 }}>
+              <IncomeAreaChart slot="week" series={seriesData}/>
+            </Box>
+          </MainCard>
+        </Grid>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default ServiceDetailsPage;
