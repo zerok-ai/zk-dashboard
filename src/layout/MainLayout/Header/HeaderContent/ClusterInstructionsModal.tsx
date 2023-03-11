6516a42b041863ea9421d612f8ef08983b446467
Shivam Nagar <shivam.nagar@outlook.com>
2023-03-11 07:47:24 +0530

    Add cluster styling fixes

--- src/layout/MainLayout/Header/HeaderContent/ClusterInstructionsModal.tsx
+++ src/layout/MainLayout/Header/HeaderContent/ClusterInstructionsModal.tsx
@@ -40,14 +40,16 @@ const steps = [
       <>
         For each ad campaign that you create, you can control how much you're willing to spend on clicks and conversions, which networks and
         geographical locations you want your ads to show on, and more.
-        <CopyBlock
-          text={installCommand.code}
-          language={installCommand.language}
-          showLineNumbers={installCommand.showLineNumbers}
-          theme={installCommand.theme}
-          wrapLines
-          codeBlock
-        />
+        <Box sx={{ my: 2 }}>
+          <CopyBlock
+            text={installCommand.code}
+            language={installCommand.language}
+            showLineNumbers={installCommand.showLineNumbers}
+            theme={installCommand.theme}
+            wrapLines
+            codeBlock
+          />
+        </Box>
       </>
     )
   },
@@ -56,14 +58,16 @@ const steps = [
     description: (
       <>
         An ad group contains one or more ads which target a shared set of keywords.
-        <CopyBlock
-          text={setupCommand.code}
-          language={setupCommand.language}
-          showLineNumbers={setupCommand.showLineNumbers}
-          theme={setupCommand.theme}
-          wrapLines
-          codeBlock
-        />
+        <Box sx={{ my: 2 }}>
+          <CopyBlock
+            text={setupCommand.code}
+            language={setupCommand.language}
+            showLineNumbers={setupCommand.showLineNumbers}
+            theme={setupCommand.theme}
+            wrapLines
+            codeBlock
+          />
+        </Box>
       </>
     )
   },
@@ -71,7 +75,9 @@ const steps = [
     label: <Typography variant="h5">Installation</Typography>,
     description: (
       <>
-        <LinearProgress />
+        <Box sx={{ my: 2 }}>
+          <LinearProgress />
+        </Box>
         Try out different ad text to see what brings in the most customers, and learn how to enhance your ads using features like ad
         extensions. If you run into any problems with your ads, find out how to tell if they're running and how to resolve approval issues.
       </>
